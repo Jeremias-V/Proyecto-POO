558d29d7bed84fd68cde2c621212a8b5c2732bb8
Jeremias-V <jeremias.villalobos.t@gmail.com>
2020-12-06 12:46:13 -0500

    MVC new Project

---
+++ TiendaCampesinos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TiendaCampesinos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
